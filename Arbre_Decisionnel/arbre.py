3b973856890a98e6a7d7620c21ce408b54f1a4c8
Cherica <riantsoacherica01@gmail.com>
2026-08-25 15:00:03 +0300

    test de gain d'info

--- Arbre_Decisionnel/arbre.py
+++ Arbre_Decisionnel/arbre.py
@@ -7,3 +7,17 @@ for p in proportion :
     if p!= 0 :
         entropie = entropie - p * math.log2(p)
 print("entropie=",entropie)
+entropie_parent = 1.0
+
+gain = entropie_parent - (
+    (4 / 10) * 0.811 +
+    (2 / 10) * 0 +
+    (4 / 10) * 1
+)
+
+print("Gain d'information =", gain)
+
+
+for z in [-2, 0, 2]:
+    resultat = 1 / (1 + math.exp(-z))
+    print("z =", z, "=>", resultat)
